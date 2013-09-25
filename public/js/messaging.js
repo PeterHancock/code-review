c76349fe96e9131ccd41155a425434971fde02c2
Peter Hancock <peter.hancock@gmail.com>
2013-09-25 04:09:39 -0400

    WIP

---
+++ public/js/messaging.js
@@ -0,0 +1,25 @@
+(function(){
+    var MessageService = function(firebaseRef, user) {
+        this._firebaseRef = firebaseRef;
+        this._user = user;
+    };
+    _(MessageService.protortype).extend({
+        send: function(to, body) {
+            var message = {
+                from: {
+                    id: this._user.id,
+                    uuid: this._user.uuid,
+                    email : this._user.email
+                },
+                uuid: to.uuid,
+                message: body
+            };
+            this._firebaseRef.child('users/' + to.id + '/messages').push(message);
+        },
+        reply: function(message, body) {
+            this.send(message.from, body);
+        }
+
+    });
+    window.MessageService = MessageService;
+}).call(this);
